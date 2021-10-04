694b3b958e5622b8b837569f055410ccf54886e0
noriaki.kishida <kishida@dobocreate.jp>
2021-10-05 00:24:15 +0900

    21.10.05　HomeViewControllerにTableViewを追加

---
+++ TunnelManagementApp/HomeViewController.swift
@@ -0,0 +1,65 @@
+//
+//  HomeViewController.swift
+//  TunnelManagementApp
+//
+//  Created by 岸田展明 on 2021/10/04.
+//
+
+import UIKit
+
+// HomeViewControllerは、UIViewControllerを継承するとともに、UITableViewDelegateとUITableViewDataSourceのプロトコルに準拠する
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // デリゲートメソッド：データ数を返す関数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 // 5個のデータがあるという意味
+    }
+
+    // デリゲートメソッド：セルの表示内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        // 値を設定する.
+        cell.textLabel!.text = "Row \(indexPath.row)"
+
+        return cell
+    }
+    
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+        return .delete
+    }
+
+    // Delete ボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    }
+    
+    // 画面遷移後に１度呼ばれる
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // デリゲートの指定。ここで、selfとはViewController
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TunnelManagementApp/kirihaRecord.swift
@@ -0,0 +1,30 @@
+//
+//  kirihaRecord.swift
+//  TunnelManagementApp
+//
+//  Created by 岸田展明 on 2021/10/04.
+//
+
+import RealmSwift
+
+class kirihaRecord: Object{
+    
+    // 管理用ID。プライマリーキー（データを一意に識別するためのID）
+    @objc dynamic var id = 0
+    
+    // 測点
+    @objc dynamic var stationNo = ""
+    
+    // 日付
+    @objc dynamic var date = Date()
+    
+    // 支保パターン
+    @objc dynamic var pattern = ""
+    
+    // idをプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        
+        return "id"
+    }
+    
+}
