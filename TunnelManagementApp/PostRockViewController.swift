f47755176af4258b3589c4b8c2132ee49d68cf75
noriaki.kishida <kishida@dobocreate.jp>
2021-10-10 07:50:43 +0900

    21.10.10　変更前保存

--- TunnelManagementApp/PostRockViewController.swift
+++ TunnelManagementApp/PostRockViewController.swift
@@ -20,9 +20,12 @@ class PostRockViewController: UIViewController {
     @IBOutlet weak var resultUploadButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     
+
     let rockNameList: [String] = ["花崗岩", "粘板岩", "砂岩"]
     
     var rockNameMenu: [UIAction] = []
+    
+    
     
     func addMenuToButton(){
         
@@ -43,11 +46,32 @@ class PostRockViewController: UIViewController {
         rockSelectButton.showsMenuAsPrimaryAction = true
     }
     
+
+    @IBAction func rockSelectButton2(_ sender: UIButton) {
+        
+        print("rockNameList:\(rockNameList.count)")
+        
+        for i in 0 ..< rockNameList.count {
+            rockNameMenu.append(UIAction(title: rockNameList[i], image: nil) { (action) in
+                
+                self.rockNameLabel.text = self.rockNameList[i]
+                
+                print(self.rockNameLabel.text!)
+            })
+        }
+        
+        let menu = UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: rockNameMenu)
+        
+        rockSelectButton.menu = menu
+        rockSelectButton.showsMenuAsPrimaryAction = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imageView.image = image
         
-        addMenuToButton()
+        // addMenuToButton()
     }
 }
